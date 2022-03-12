6d2f95d0611f6ee71f9883ecd399cd4e31657449
hugosenari <hugosenari@gmail.com>
2022-03-12 02:10:57 -0300

    chore: re index hex

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A simple hex package for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-hex-master.flake = false;
+  inputs.src-hex-master.owner = "esbullington";
+  inputs.src-hex-master.ref   = "refs/heads/master";
+  inputs.src-hex-master.repo  = "nimrod-hex";
+  inputs.src-hex-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-hex-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-hex-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
